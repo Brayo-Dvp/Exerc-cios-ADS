c3f696f91ffe61fc398d7511ec606144264083ff
Brayo-Dvp <bonness.bryan@gmail.com>
2025-09-02 21:24:36 -0300

    Add files via upload

---
+++ Exercício 5 - Python.py
@@ -0,0 +1,5 @@
+a = int(input("Digite um número!"))
+b = int(input("Digite outro número!"))
+
+soma = a + b
+print(f"{a} mais {b} é igual a {soma}!")
---
+++ Exercício 6 - Python.py
@@ -0,0 +1,5 @@
+a = int(input("Qual o dia do nascimento? (De 01 a 31)"))
+b = int(input("Qual o mês do nascimento? (de 01 a 12)"))
+c = int(input("Qual o ano do nascimento?"))
+
+print(f"{a}/{b}/{c}")
